d713dab371df6a478b42c5171c021bfc98c80318
Javier Medina <javieralejandro.o2506@gmail.com>
2025-10-31 12:06:14 -0600

    Esqueleto v.1

--- Docs/#Sistema de inventario - Pseudocodigo.py
+++ Docs/#Sistema de inventario - Pseudocodigo.py
@@ -1,1 +1,25 @@
 #Sistema de inventario - Pseudocodigo
+
+# SISTEMA DE INVENTARIO - TIENDA AUTOMOTRIZ
+
+# Paso 1: Preguntar cuántos productos se registrarán
+# num_productos = int(input("¿Cuántos productos se registrarán? "))
+
+# Paso 2: Variables acumuladoras
+# inventario 
+# total_productos 
+# total_valor_inventario 
+# producto_menor_stock 
+# producto_mayor_stock 
+# menor_stock 
+# mayor_stock 
+
+# Paso 3: Ciclo para registrar cada producto
+# Entradas del usuario (input de nombre, codigo, marca, precio, cantidad y lugar)
+# Validaciones (el precio ni la cantidad pueden ser negativos)
+# Calcular valor total del producto (precio * cantidad)
+# Acumular totales (valor total del inventario y cantidad total de productos)
+# info del resumen (inventario += ...)
+# Identificar producto con menor y mayor stock
+
+# Paso 4: Imprimir el reporte final con datos necesarios utilizando print
